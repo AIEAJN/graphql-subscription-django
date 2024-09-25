20816f51591308039066cee8315bf3285b7f12a9
AIEAJN <47372918+AIEAJN@users.noreply.github.com>
2024-09-25 17:38:40 +0000

    add graphql url

--- api/api/urls.py
+++ api/api/urls.py
@@ -23,5 +23,4 @@ from graphene_django.views import GraphQLView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True)), name='graphql'),
-
 ]
