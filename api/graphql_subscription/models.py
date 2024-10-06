ced8ec65fe9e3a5fc1409df1cb179a14afcecbc5
AIEAJN <47372918+AIEAJN@users.noreply.github.com>
2024-10-06 13:10:07 +0000

    add test : test_add_existing_manga, test_add_manga

--- api/graphql_subscription/models.py
+++ api/graphql_subscription/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Manga(models.Model):
+    name = models.CharField(max_length=255, default=None, blank=False)
+    synopsis = models.CharField(max_length=255, default=None, blank=True)
+    author = models.CharField(max_length=100, default=None, blank=False)
+    created_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    
+    class Meta:
+        managed = True
+        db_table = 'manga'
+        unique_together = (("name", "author"),)
+    
--- api/graphql_subscription/tests.py
+++ api/graphql_subscription/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from graphene_django.utils.testing import GraphQLTestCase
 from django.urls import reverse
+from .models import Manga
 
 
 class URLTests(TestCase):
@@ -22,3 +23,50 @@ class QueryTests(GraphQLTestCase):
         content = response.json()
         self.assertResponseNoErrors(response)
         self.assertEqual(content['data']['greeting'], 'Hello World')
+        
+        
+class MutationsTests(GraphQLTestCase):
+    def test_add_manga(self):
+        response = self.query(
+            '''
+            mutation addManga(name="Naruto", synopsis="synopsis", author="Masashi Kishimoto"){
+                success
+                message
+                manga{
+                    name
+                    author
+                }
+            }
+            '''
+        )
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertResponseNoErrors(response)
+        content = response.json()
+        self.assertTrue(content['data']['success'])
+        self.assertIsNone(content['data']['message'])
+        self.assertEqual(content['data']['manga']["name"], "Naruto")
+        self.assertEqual(content['data']['manga']["author"], "Masashi Kishimoto")
+        
+    def test_add_existing_manga(self):
+        Manga.objects.create(name="Naruto", synopsis="synopsis", author="Masashi Kishimoto")
+        
+        response = self.query(
+            '''
+            mutation addManga(name="Naruto", synopsis="synopsis", author="Masashi Kishimoto"){
+                success
+                message
+                manga{
+                    name
+                    author
+                }
+            }
+            '''
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertResponseHasErrors(response)
+        content = response.json()
+        self.assertFalse(content['data']['success'])
+        self.assertIsNotNone(content['data']['message'])
+        self.assertIsNone(content['data']['manga'])
+        
